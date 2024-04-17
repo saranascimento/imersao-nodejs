21320b85dcdd740b261c8f621cc7ba134aad7930
Sara Gomes <sarah.gomes80@hotmail.com>
2024-04-16 23:37:01 -0300

    Manipulando arquivos - UPDATE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -1,24 +1,22 @@
 const { deepEqual, ok } = require('assert');
 const database = require('./database');
+
 const DEFAULT_ITEM_CADASTRAR = {
   nome: 'Flash',
   poder: 'Speed',
   id: 1,
 };
 
-// destructor;
-// const posicaoUm = resultado[0] ou [resultado]
-// const posicaoUmEDois = [posica1, posicao2]
+const DEFAULT_ITEM_ATUALIZAR = {
+  nome: 'Lanterna Verde',
+  poder: 'Energia do Anel',
+  id: 2,
+};
 
 describe('Suite de manipulação de Herois', () => {
   before(async () => {
     await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
-  });
-
-  it('deve pesquisar um heroi, usando arquivos', async () => {
-    const expected = DEFAULT_ITEM_CADASTRAR;
-    const [resultado] = await database.listar(expected.id);
-    deepEqual(resultado, expected);
+    await database.cadastrar(DEFAULT_ITEM_ATUALIZAR);
   });
 
   it('deve cadastrar um heroi, usando arquivos', async () => {
@@ -28,7 +26,32 @@ describe('Suite de manipulação de Herois', () => {
     deepEqual(actual, expected);
   });
 
-  it('deve remover um heroi, usando arquivos', async () => {
+  it('deve pesquisar um heroi, usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const [resultado] = await database.listar(expected.id);
+    deepEqual(resultado, expected);
+  });
+
+  it('deve atualizar um heroi pelo id', async () => {
+    const expected = {
+      ...DEFAULT_ITEM_ATUALIZAR,
+      nome: 'Batman',
+      poder: 'Dinheiro',
+    };
+
+    const novoDado = {
+      nome: 'Batman',
+      poder: 'Dinheiro',
+    };
+
+    await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado);
+
+    const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id);
+
+    deepEqual(resultado, expected);
+  });
+
+  it('deve remover um heroi pelo id', async () => {
     const expected = true;
     const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
     deepEqual(resultado, expected);
