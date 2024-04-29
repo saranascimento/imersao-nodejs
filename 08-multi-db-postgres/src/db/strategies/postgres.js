f99ac6e3e8aacb730de8022dc0486125845e394b
Sara Gomes <sarah.gomes80@hotmail.com>
2024-04-29 08:29:03 -0300

    Atualizando Heróis - UPDATE

--- 08-multi-db-postgres/src/db/strategies/postgres.js
+++ 08-multi-db-postgres/src/db/strategies/postgres.js
@@ -56,6 +56,10 @@ class Postgres extends ICrud {
     return await this._herois.findAll({ where: item, raw: true });
   }
 
+  async update(id, item) {
+    return await this._herois.update(item, { where: { id: id } });
+  }
+
   async connect() {
     this._driver = new Sequelize('heros', 'user', 'senha', {
       host: 'localhost',
--- 08-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ 08-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -8,11 +8,17 @@ const MOCK_HEROI_CADASTRAR = {
   poder: 'flexas',
 };
 
+const MOCK_HEROI_ATUALIZAR = {
+  nome: 'Batman',
+  poder: 'Dinheiro',
+};
+
 describe('Postgres Strategy', function () {
   this.timeout(Infinity);
 
   this.beforeAll(async function () {
     await context.connect();
+    await context.create(MOCK_HEROI_ATUALIZAR);
   });
 
   it('PostgresSQL Connection', async function () {
@@ -28,10 +34,31 @@ describe('Postgres Strategy', function () {
 
   it('listar', async function () {
     const [result] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome });
-    // pegar a primeira posicao
-    // const posicaoZero = result[0]
-    //const [posicao1, posicao2] = ['esse é o 1', 'esse é o 2']
     delete result.id;
     assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+  });
+
+  it('atualizar', async function () {
+    const [itemAtualizar] = await context.read({
+      nome: MOCK_HEROI_CADASTRAR.nome,
+    });
+    const novoItem = {
+      ...MOCK_HEROI_ATUALIZAR,
+      nome: 'Mulher Maravilha',
+    };
+    const [result] = await context.update(itemAtualizar.id, novoItem);
+    const [itemAtualizado] = await context.read({ id: itemAtualizar.id });
+    assert.deepEqual(result, 1);
+    assert.deepEqual(itemAtualizado.nome, novoItem.nome);
+    /*
+    No Javascript temos uma tecnca chamada rest/spread que é um metodo usado para mergear objetos ou separa-los
+    {
+      nome: 'Batman',
+      poder: 'Dinheiro'
+    },
+    {
+      dataNascimento: '1998-01-01'
+    }
+    */
   });
 });
