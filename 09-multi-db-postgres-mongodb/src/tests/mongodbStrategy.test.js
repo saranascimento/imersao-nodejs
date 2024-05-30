4d7439dc671545d18085c723e7326bcf483e1807
Sara Gomes <sarah.gomes80@hotmail.com>
2024-05-30 17:52:57 -0300

    Cadastrando Heróis - CREATE

--- 09-multi-db-postgres-mongodb/src/tests/mongodbStrategy.test.js
+++ 09-multi-db-postgres-mongodb/src/tests/mongodbStrategy.test.js
@@ -2,6 +2,11 @@ const assert = require('assert');
 const MongoDb = require('../db/strategies/mongodb');
 const Context = require('../db/strategies/base/contextStrategy');
 
+const MOCK_HEROI_CADASTRAR = {
+  nome: 'Mulher Maravilha',
+  poder: 'Laço',
+};
+
 const context = new Context(new MongoDb());
 describe('MongoDB Suite de testes', function () {
   this.beforeAll(async () => {
@@ -14,5 +19,10 @@ describe('MongoDB Suite de testes', function () {
     const expected = 'Conectado';
 
     assert.deepEqual(result, expected);
+  });
+
+  it('cadastrar', async () => {
+    const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR);
+    assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR);
   });
 });
