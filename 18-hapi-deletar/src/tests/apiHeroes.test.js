ae53b14e20954c4d9847c2a54243345abc397ff3
Sara Gomes <sarah.gomes80@hotmail.com>
2024-06-09 23:34:12 -0300

    Removendo Heróis - DELETE

---
+++ 18-hapi-deletar/src/tests/apiHeroes.test.js
@@ -0,0 +1,198 @@
+const assert = require('assert');
+const api = require('./../api');
+
+let app = {};
+const MOCK_HEROI_CADASTRAR = {
+  nome: 'Chapolin Colorado',
+  poder: 'Marreta Bionica',
+};
+
+const MOCK_HEROI_INICIAL = {
+  nome: 'Gavião Negro',
+  poder: 'A mira',
+};
+
+let MOCK_ID = '';
+describe('Suite de testes de API Heroes', function () {
+  this.beforeAll(async () => {
+    app = await api;
+    const result = await app.inject({
+      method: 'POST',
+      url: '/herois',
+      payload: JSON.stringify(MOCK_HEROI_INICIAL),
+    });
+    const dados = JSON.parse(result.payload);
+    MOCK_ID = dados._id;
+  });
+
+  it('Listar /herois', async () => {
+    const result = await app.inject({
+      method: 'GET',
+      url: '/herois?skip=0&limit=20',
+    });
+
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+
+    assert.deepEqual(statusCode, 200);
+    assert.ok(Array.isArray(dados));
+  });
+
+  it('listar/herois - deve retornar somente 3 registros', async () => {
+    TAMANHO_LIMITE = 3;
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`,
+    });
+
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados.length === TAMANHO_LIMITE);
+  });
+
+  it('listar/herois - deve retornar um erro com limit incorreto', async () => {
+    TAMANHO_LIMITE = 'aeee';
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`,
+    });
+
+    const errorResult = {
+      statusCode: 400,
+      error: 'Bad Request',
+      message: '"limit" must be a number',
+      validation: {
+        source: 'query',
+        keys: ['limit'],
+      },
+    };
+
+    assert.deepEqual(result.statusCode, 400);
+    assert.deepEqual(result.payload, JSON.stringify(errorResult));
+  });
+
+  it('listar - GET /herois - deve filtrar um item', async () => {
+    const NAME = 'Homem Aranha-1717348206227';
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=20&nome=${NAME}`,
+    });
+
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.deepEqual(dados[0].nome, NAME);
+  });
+
+  it('cadastrar - POST /herois', async () => {
+    const result = await app.inject({
+      method: 'POST',
+      url: '/herois',
+      payload: MOCK_HEROI_CADASTRAR,
+    });
+
+    const statusCode = result.statusCode;
+    const { message, _id } = JSON.parse(result.payload);
+
+    assert.ok(statusCode === 200);
+    assert.notStrictEqual(_id, undefined);
+    assert.deepEqual(message, 'Heroi cadastrado com sucesso!');
+  });
+
+  it('atualizar PATCH - /herois/:id', async () => {
+    const _id = MOCK_ID;
+    const expected = {
+      poder: 'Super Mira',
+    };
+
+    const result = await app.inject({
+      method: 'PATCH',
+      url: `/herois/${_id}`,
+      payload: JSON.stringify(expected),
+    });
+
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+
+    assert.ok(statusCode === 200);
+    assert.deepEqual(dados.message, 'Heroi atualizado com sucesso!');
+  });
+
+  it('atualizar PATCH - /herois/:id - não deve atualizar com id incorreto', async () => {
+    const _id = '66546c97d80948ec94958b2b';
+
+    const result = await app.inject({
+      method: 'PATCH',
+      url: `/herois/${_id}`,
+      payload: JSON.stringify({
+        poder: 'Super Mira',
+      }),
+    });
+
+    const expected = {
+      statusCode: 412,
+      error: 'Precondition Failed',
+      message: 'Id não encontrado no banco',
+    };
+
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+    console.log('dados', dados);
+
+    assert.ok(statusCode === 412);
+    assert.deepEqual(dados, expected);
+  });
+
+  it('remover - DELETE - /herois/:id', async () => {
+    const _id = MOCK_ID;
+
+    const result = await app.inject({
+      method: 'DELETE',
+      url: `/herois/${_id}`,
+    });
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+
+    assert.ok(statusCode === 200);
+    assert.deepEqual(dados.message, 'Heroi Removido com sucesso!');
+  });
+
+  it('remover - DELETE - /herois/:id não deve remover', async () => {
+    const _id = '66546c97d80948ec94958b2b';
+
+    const result = await app.inject({
+      method: 'DELETE',
+      url: `/herois/${_id}`,
+    });
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+    const expected = {
+      statusCode: 412,
+      error: 'Precondition Failed',
+      message: 'Id não encontrado no banco',
+    };
+
+    assert.ok(statusCode === 412);
+    assert.deepEqual(dados, expected);
+  });
+
+  it('remover - DELETE - /herois/:id não deve remover com id invalido', async () => {
+    const _id = 'ID_INVALIDO';
+
+    const result = await app.inject({
+      method: 'DELETE',
+      url: `/herois/${_id}`,
+    });
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+    const expected = {
+      error: 'Internal Server Error',
+      message: 'An internal server error occurred',
+      statusCode: 500,
+    };
+
+    assert.ok(statusCode === 500);
+    assert.deepEqual(dados, expected);
+  });
+});
